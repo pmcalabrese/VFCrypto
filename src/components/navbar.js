ede19c52c706cb58ca35e635354203d32cbb3986
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2018-12-06 17:41:12 +0100

    better navbar for home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import USD from  "../../node_modules/cryptocurrency-icons/32/color/usd.png"
+import EUR from  "../../node_modules/cryptocurrency-icons/32/color/eur.png"
+import GBP from  "../../node_modules/cryptocurrency-icons/32/color/gbp.png"
+import JPY from  "../../node_modules/cryptocurrency-icons/32/color/jpy.png"
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  changeCurrentCurrency(selected_currency) {
+    this.props.dispatch({
+      type: "CHANGE_CURRENT_CURRENCY",
+      payload: selected_currency
+    });
+  } 
+
   render() {
+
+    const { current_currency } = this.props;
+
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
@@ -25,18 +43,17 @@ class Navbar extends Component {
         </div>
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
-            <h1>VFCrypto</h1>
+            <Link to="/"><h1>VFCrypto</h1></Link>
           </div>
           <div className="navbar-end">
             <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Currency</a>
-
+              <a className="navbar-link">Currency ({current_currency})</a>
               <div className="navbar-dropdown">
-                <a className="navbar-item">USD</a>
-                <a className="navbar-item">GBP</a>
-                <a className="navbar-item">EUR</a>
-                <a className="navbar-item">JPY</a>
-                <a className="navbar-item">KRW</a>
+                <a onClick={() => this.changeCurrentCurrency("GBP")} className="navbar-item"><img className="select-icon" src={GBP} />GBP</a>
+                <a onClick={() => this.changeCurrentCurrency("USD")} className="navbar-item"><img className="select-icon" src={USD} />USD</a>
+                <a onClick={() => this.changeCurrentCurrency("EUR")} className="navbar-item"><img className="select-icon" src={EUR} />EUR</a>
+                <a onClick={() => this.changeCurrentCurrency("JPY")} className="navbar-item"><img className="select-icon" src={JPY} />JPY</a>
+                <a onClick={() => this.changeCurrentCurrency("KRW")} className="navbar-item"><img className="select-icon" src={USD} />KRW</a>
               </div>
             </div>
           </div>
@@ -46,4 +63,11 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+function mapStateToProps(state) {
+  return {
+    // the key is the props
+    current_currency: state.current_currency
+  };
+}
+
+export default connect(mapStateToProps)(Navbar);

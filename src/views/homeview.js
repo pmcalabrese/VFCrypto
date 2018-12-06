de226633d9df98586dc03aece654d3a04ee12d30
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2018-12-06 17:42:15 +0100

    progress on homeview

--- src/views/homeview.js
+++ src/views/homeview.js
@@ -1,92 +1,146 @@
-import React, { Component } from 'react';
-import Coinmarketcap from "../services/coinmarketcap";
-import { formatMoney } from "../utilis";
-import BTC from  "../../node_modules/cryptocurrency-icons/32/color/btc.png"
-import XRP from  "../../node_modules/cryptocurrency-icons/32/color/xrp.png"
-import ETH from  "../../node_modules/cryptocurrency-icons/32/color/eth.png"
-import XLM from "../../node_modules/cryptocurrency-icons/32/color/xlm.png"
-import BCH from "../../node_modules/cryptocurrency-icons/32/color/bch.png"
-import EOS from "../../node_modules/cryptocurrency-icons/32/color/eos.png"
-import LTC from "../../node_modules/cryptocurrency-icons/32/color/ltc.png"
-// import USDT from "../../node_modules/cryptocurrency-icons/32/color/ustd.png"
-// import BSV from "../../node_modules/cryptocurrency-icons/32/color/bsv.png"
-import ADA from "../../node_modules/cryptocurrency-icons/32/color/ada.png"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Navbar from "../components/navbar";
 
-const ICONS = {
-    BTC,
-    XRP,
-    ETH,
-    XLM,
-    BCH,
-    EOS,
-    LTC,
-    // USDT,
-    // BSV,
-    ADA
-}
+// import Coinmarketcap from "../services/coinmarketcap";
+import Coingecko from "../services/coingecko";
+import { formatMoney, ICONS, CURRENCY_SYMBOLS } from "../utilis";
 
 class Homeview extends Component {
-    constructor(props) {
-        super(props);
-        this.interval = undefined;
-        this.state = { 
-            currencies: [],
-            last_update: null
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.interval = undefined;
+    this.state = {
+      currencies: [],
+      last_update: null
+    };
+  }
 
-    updateCurrencies = () => {
-        Coinmarketcap.getCurrencies().then((data) => {
-            console.log("data", data);
-            const now = new Date();
-            this.setState({
-                last_update: now.toString(),
-                currencies: data
-            })
-        })
-    }
+  componentWillReceiveProps(props) {
+    this.setState({
+      loading: true
+    });
+    this.updateCurrencies(props.current_currency);
+  }
 
-    componentDidMount() {
-        this.updateCurrencies();
-        this.interval = setInterval(this.updateCurrencies, 60000)
-    }
+  updateCurrencies = currency_to_convert => {
+    return Coingecko.getCurrencies({
+      vs_currency: currency_to_convert,
+      per_page: 10,
+      page: 1
+    }).then(currencies => {
+      const now = new Date();
+      this.setState({
+        last_update: now.toString(),
+        currencies,
+        loading: false
+      });
+    });
+  };
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+  componentDidMount() {
+    this.updateCurrencies(this.props.current_currency);
+    this.interval = setInterval(() => {
+      this.updateCurrencies(this.props.current_currency);
+    }, 60000);
+  }
 
-    render() { 
-        const { currencies, last_update } = this.state;
-        return ( 
-            <div className="section">
-                <table className="table is-fullwidth">
-                    <thead>
-                        <tr>
-                            <th>CRYPTOCURRENCY</th>
-                            <th>PRICE</th>
-                            <th>MARKET CAP</th>
-                            <th>24H CHANGE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            currencies.length !== 0 ?
-                            currencies.map((currency) => (
-                                <tr key={currency.name}>
-                                    <td className="td-center" title={ currency.symbol }><span className="currency-rank">{ currency.rank }</span><img src={ICONS[currency.symbol]} /><span className="currency-name"> { currency.name }</span></td>
-                                    <td>{ formatMoney(currency.price_usd) }</td>
-                                    <td><small>{ formatMoney(currency.market_cap_usd) }</small></td>
-                                    <td className={ currency.percent_change_24h < 0 ? "negative-cap" : "positive-cap" }><b><small>{ currency.percent_change_24h } %</small></b></td>
-                                </tr>
-                            )) : "loading"
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { currencies, last_update, loading } = this.state;
+    const { current_currency } = this.props;
+
+    return (
+      <React.Fragment>
+        <div>
+          <Navbar />
+        </div>
+        <div className="section">
+          <table className="table is-fullwidth">
+            <thead>
+              <tr>
+                <th>CRYPTOCURRENCY</th>
+                <th>PRICE</th>
+                <th>MARKET CAP</th>
+                <th>24H CHANGE</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currencies.length !== 0
+                ? currencies.map(currency => (
+                    <tr key={currency.name}>
+                      <td className="td-center" title={currency.symbol}>
+                        <span className="currency-rank">{currency.rank}</span>
+                        <Link to={`currency/${currency.name.toLowerCase()}`}>
+                          <img
+                            alt={currency.name}
+                            src={ICONS[currency.symbol.toUpperCase()]}
+                          />
+                          <span className="currency-name">
+                            {" "}
+                            {currency.name}
+                          </span>
+                        </Link>
+                      </td>
+                      <td>
+                        {!loading ? (
+                          <span>
+                            <span>{CURRENCY_SYMBOLS[current_currency]}</span>
+                            <span>{formatMoney(currency.current_price)}</span>
+                          </span>
+                        ) : (
+                          "loading"
+                        )}
+                      </td>
+                      <td>
+                        {!loading ? (
+                          <small>
+                            <span>{CURRENCY_SYMBOLS[current_currency]}</span>
+                            {formatMoney(currency.market_cap)}
+                          </small>
+                        ) : (
+                          "loading"
+                        )}
+                      </td>
+                      <td
+                        className={
+                          currency.market_cap_change_percentage_24h < 0
+                            ? "negative-cap"
+                            : "positive-cap"
                         }
-                        
-                    </tbody>
-                </table>
-                <span><b>Last update</b> { last_update }</span>
-            </div>
-         );
-    }
+                      >
+                        {!loading ? (
+                          <b>
+                            <small>
+                              {currency.market_cap_change_percentage_24h} %
+                            </small>
+                          </b>
+                        ) : (
+                          "loading"
+                        )}
+                      </td>
+                    </tr>
+                  ))
+                : "loading"}
+            </tbody>
+          </table>
+          <span>
+            <b>Last update</b> {last_update}
+          </span>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
-export default Homeview;
+
+function mapStateToProps(state) {
+  return {
+    current_currency: state.current_currency
+  };
+}
+
+export default connect(mapStateToProps)(Homeview);

16c2b2c2545efffb7ec62e668295d07f465eecb8
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2018-12-06 17:40:43 +0100

    add Redux for handle global changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import '../src/style/style.scss';
-import Navbar from './components/navbar';
-import Homeview from './views/homeview';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { reducer } from "../src/reducers/reducer"
 
+import "../src/style/style.scss";
+import Homeview from "./views/homeview";
+import DetailView from "./views/detailview";
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <section className="section">
-          <div className="container">
-            <Navbar />
+      <Provider store={store}>
+        <Router>
+          <div className="container is-fluid">
             <Route path="/" exact component={Homeview} />
+            <Route path="/currency/:coin" exact component={DetailView} />
           </div>
-        </section>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
